c80d67215694db3e2699663465c6e18ce0281791
ChaosMaster11 <155442782+ChaosMaster11@users.noreply.github.com>
2024-01-10 17:02:33 +0200

    Add files via upload

---
+++ JonDou9000.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,31 @@
+﻿using JonDou9000.TaskPlanner.Domain.Models;
+
+namespace JonDou9000.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        public WorkItem[] CreatePlan(WorkItem[] items)
+        {
+            List<WorkItem> itemsAsList = items.ToList();
+            itemsAsList.Sort(CompareWorkItems);
+            return itemsAsList.ToArray();
+        }
+
+        private static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
+        {
+            int priorityComparison = secondItem.Priority.CompareTo(firstItem.Priority);
+            if (priorityComparison != 0)
+            {
+                return priorityComparison;
+            }
+
+            int dueDateComparison = firstItem.DueDate.CompareTo(secondItem.DueDate);
+            if (dueDateComparison != 0)
+            {
+                return dueDateComparison;
+            }
+
+            return string.Compare(firstItem.Title, secondItem.Title, StringComparison.Ordinal);
+        }
+    }
+}
